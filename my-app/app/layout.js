0ec1288d02f1562595b086cf8d7268e19b009ee6
Aquib Khan <aquibkhanstud@gmail.com>
2025-04-18 02:42:52 +0530

    Done with major features

--- my-app/app/layout.js
+++ my-app/app/layout.js
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@/components/ui/theme-provider";
 import Header from "@/components/ui/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({ children }) {
             {/* header */}
             <Header />
             <main className="min-h-screen">{children}</main>
+            <Toaster richColors/>
 
             {/* footer */}
             <footer className="bg-muted/50 py-12">
